e110efafdf34a2118d7b8026b5917dad7fef28f2
vchornyy12 <vchornyy12@gmail.com>
2021-12-31 09:14:22 +0200

    added validation for owner - firstName/lastName requirement and added min/max name's length

--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title: string;
 
   constructor() {
     this.title = 'Device Service';
   }
 }
+
