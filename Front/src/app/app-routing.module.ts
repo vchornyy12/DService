97805214041b6db5c658aa0e6b8a80f6069f8597
Voland <vchornyy@ukr.net>
2022-01-23 17:18:00 +0200

    unique device code validation

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -2,14 +2,18 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {OwnerComponent} from "./owner/owner.component";
 import {DeviceComponent} from "./device/device.component";
+import {PageNotFoundComponent} from "src/others/pageNotFound.component";
 
 const routes: Routes = [
-  { path: 'owners', component: OwnerComponent },
-  { path: 'devices', component: DeviceComponent },
-  ];
+    {path: 'owners', component: OwnerComponent},
+    {path: 'devices', component: DeviceComponent},
+    {path: '', redirectTo: '/owners', pathMatch: 'full'},
+    {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -1,35 +1,36 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {ReactiveFormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {DeviceComponent} from './device/device.component';
 import {OwnerComponent} from './owner/owner.component';
 import {HttpClientModule} from "@angular/common/http";
-import {FormsModule} from "@angular/forms";
 import {OwnerService} from "./owner/owner.service";
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NgxPaginationModule } from 'ngx-pagination';
-import { SortDirective } from './directive/sort.directive';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {NgxPaginationModule} from 'ngx-pagination';
+import {SortDirective} from './directive/sort.directive';
+import {PageNotFoundComponent} from "../others/pageNotFound.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DeviceComponent,
-    OwnerComponent,
-    SortDirective
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule,
-    NgbModule,
-    NgxPaginationModule
-  ],
-  providers: [OwnerService],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        DeviceComponent,
+        OwnerComponent,
+        SortDirective,
+        PageNotFoundComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule,
+        NgbModule,
+        NgxPaginationModule
+    ],
+    providers: [OwnerService],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 }
